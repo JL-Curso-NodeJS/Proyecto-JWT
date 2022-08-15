e5fd2272ebd5010c5a1ed0ac91303e5d01da38ed
OmgCopito95 <july1995bolt@gmail.com>
2022-08-15 20:31:42 -0300

    Update errors.js

--- src/const/errors.js
+++ src/const/errors.js
@@ -15,6 +15,18 @@ module.exports = {
     'ArchivoInexistente': {
         code: 1003,
         message: 'El archivo no existe'
+    },
+    'CredencialesInvalidas': {
+        code: 1004,
+        message: 'Credenciales inválidas'
+    },
+    'UsuarioNoAutorizado': {
+        code: 1005,
+        message: 'Usuario no autorizado'
+    },
+    'SesionExpirada': {
+        code: 1006,
+        message: 'Sesión expirada'
     }
 
 
