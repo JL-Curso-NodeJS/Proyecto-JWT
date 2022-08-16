4d422498474414e01154948e7106a17135e921cd
OmgCopito95 <july1995bolt@gmail.com>
2022-08-15 22:19:52 -0300

    crear usuario scheme pide pass

--- src/middlewares/schemes/usuario.scheme.js
+++ src/middlewares/schemes/usuario.scheme.js
@@ -6,7 +6,8 @@ let crearUsuario = Joi.object({
     nombre: Joi.string().required(),
     apellido: Joi.string().required(),
     email: Joi.string().email().optional(),
-    edad: Joi.number().optional()
+    edad: Joi.number().optional(),
+    password: Joi.string().required(),
 })
 
 module.exports = {
